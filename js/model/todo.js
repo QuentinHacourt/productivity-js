97d06df07d67f2f6b89d6c950f16ee8a32ad5e4f
QuentinHacourt <quentin.hacourt@gmail.com>
2022-05-24 21:26:14 +0200

    started implementing indexedDb

---
+++ js/model/todo.js
@@ -0,0 +1,8 @@
+export class TodoItem {
+  cosntructor(id, title, description, deadline) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.deadline = deadline;
+  }
+}
---
+++ js/view/todo.js
@@ -0,0 +1,15 @@
+import { addTodo } from "./controller/todo.js";
+
+// addTodo();
+
+// console.log("lmao");
+
+// const sayHi = () => {
+//   addTodo()
+//   console.log("hi");
+// }
+
+function sayHi() {
+  addTodo()
+  console.log("hi");
+}
