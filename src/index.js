5ad05eec2ec57958edca6c1819c59cc7e9371e3d
QuentinHacourt <quentin.hacourt@gmail.com>
2022-05-31 19:12:08 +0200

    removed comments

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import { PomodoroController } from "./controller/pomodoro"
 
 
 const start = async () => {
-  // isRunning = true;
   document.getElementById("start-button").disabled = true;
   document.getElementById("pause-button").disabled = false;
-  // loop();
   pomodoroController.start();
 }
-//
+
 const setPomodoroMode = async () => {
-//   setMode(25);
   document.getElementById("pomodoro-button").disabled = true;
   document.getElementById("short-break-button").disabled = false;
   document.getElementById("long-break-button").disabled = false;
@@ -18,48 +15,31 @@ const setPomodoroMode = async () => {
 }
 //
 const setShortBreakMode = async () => {
-  // setMode(5);
   document.getElementById("pomodoro-button").disabled = false;
   document.getElementById("short-break-button").disabled = true;
   document.getElementById("long-break-button").disabled = false;
   pomodoroController.setShortBreakMode();
 }
-//
+
 const setLongBreakMode = async () => {
-  // setMode(15);
   document.getElementById("pomodoro-button").disabled = false;
   document.getElementById("short-break-button").disabled = false;
   document.getElementById("long-break-button").disabled = true;
   pomodoroController.setLongBreakMode();
 }
-//
+
 const pause = async () => {
-  // isRunning = false;
   document.getElementById("start-button").disabled = false;
   document.getElementById("pause-button").disabled = true;
   pomodoroController.pause();
 }
-//
+
 const reset = async () => {
-  // setMode(mode);
   document.getElementById("start-button").disabled = false;
   document.getElementById("pause-button").disabled = true;
   pomodoroController.reset();
 }
-//
-// const printTime = async () => {
-//   await printMessage(`${await timeToString(minutesLeft)}:${await timeToString(secondsLeft)}`);
-// }
-//
-//
-// const setMode = async (mode) => {
-//   pause();
-//   mode = mode;
-//   minutesLeft = mode;
-//   secondsLeft = 0;
-//   printTime();
-// }
-//
+
 const printMessage = async (message) => {
   let timer = document.getElementById("timer");
   let timerData = document.createElement("p");
@@ -67,36 +47,6 @@ const printMessage = async (message) => {
   timer.innerHTML = "";
   timer.appendChild(timerData);
 }
-//
-// const loop = async () => {
-//   while (isRunning) {
-//       if (secondsLeft === 0 && minutesLeft === 0){
-//         printMessage("Time is over!");
-//         isRunning = false;
-//       } else if (secondsLeft === 0){
-//         minutesLeft--;
-//         secondsLeft = 59;
-//         printTime();
-//       } else {
-//         secondsLeft--;
-//         printTime();
-//       }
-//       await delay(1000);
-//   }
-// }
-//
-// const timeToString = async (time) => {
-//   if (time < 10) {
-//     return `0${time}`
-//   }
-//   return `${time}`
-// }
-//
-// const delay = async (milliseconds) => {
-//   return new Promise(resolve => {
-//     setTimeout(resolve, milliseconds);
-//   });
-// }
 
 const pomodoroController = new PomodoroController(printMessage);
 
